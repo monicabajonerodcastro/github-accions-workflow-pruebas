2b2dfc14115ffea39ce41604cb2297a03050ca6d
Monica Bajonero Dcastro <m.bajonero@uniandes.edu.co>
2022-09-21 21:23:17 -0500

    mezclado a release

--- tests/test_persona.py
+++ tests/test_persona.py
@@ -43,5 +43,5 @@ class PersonaTestCase(unittest.TestCase):
         self.assertIsInstance(self.persona1, Persona)
         self.assertNotIsInstance(self.grupo, Persona)
 
-    def test_testiiiing(self):
+    def test_testing(self):
         self.assertEqual(0, 0)
