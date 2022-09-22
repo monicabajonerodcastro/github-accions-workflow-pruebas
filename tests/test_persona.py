bec32e51125066c738c73c01b5d69ce7544d13e5
Monica Bajonero Dcastro <m.bajonero@uniandes.edu.co>
2022-09-21 20:46:10 -0500

    prueba

--- tests/test_persona.py
+++ tests/test_persona.py
@@ -44,4 +44,4 @@ class PersonaTestCase(unittest.TestCase):
         self.assertNotIsInstance(self.grupo, Persona)
 
     def test_testing(self):
-        self.assertEqual(0, 0)
+        self.assertEqual(0, 1)
